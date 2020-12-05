75ddb95641eb7fe1c77672a6541cf549d285ee4b
Abner <sbner2@gmail.com>
2020-12-05 14:07:29 -0300

    basic html and script

---
+++ dog-age.js
@@ -0,0 +1,16 @@
+const idade = document.querySelector("#idade");
+const button = document.querySelector("#calculate");
+const porte = document.querySelectorAll(".tipo-porte");
+
+
+button.onclick = () => {
+    // console.log(idade.value)
+    // porte.forEach(element => {
+    //     console.log(element.checked);
+    // });
+
+    const age = idade.value;
+
+
+}
+
