f8911e2dcdabcd290b69f2918b16b91c24757b4e
Abner <sbner2@gmail.com>
2020-12-07 18:10:33 -0300

    Finalizado JS Funcional

--- dog-age.js
+++ dog-age.js
@@ -1,16 +1,48 @@
 const idade = document.querySelector("#idade");
 const button = document.querySelector("#calculate");
 const porte = document.querySelectorAll(".tipo-porte");
-
+const resultDiv = document.querySelector("#results");
 
 button.onclick = () => {
-    // console.log(idade.value)
-    // porte.forEach(element => {
-    //     console.log(element.checked);
-    // });
 
+    // Captura a idade do cachorro
     const age = idade.value;
+    if (age == "") {
+        alert("Digite quantos anos seu cachorro tem!");
+        return
+    }
+    let porteSelecionado = "";
 
+    let formula = 0;
+    
+    // Descobre qual porte foi selecionado
+    porte.forEach(element => {
+        if (element.checked === true) {
+            porteSelecionado = element.value;
+        }
+    });
+    // Condicional que checa se a pessoa selecionou o porte
+    if (porteSelecionado === "") {
+        alert("Por favor selecione o porte do seu cachorro :)");
+        return
+    }
+    // Atribui a formula correta dependendo do porte
+    switch (porteSelecionado) {
+        case "pequeno":
+            formula = 6;
+            break;
+        case "medio":
+            formula = 7;
+            break;
+        case "grande":
+            formula = 8;
+            break;
+        
+    }
+    // Faz o calculo da idade relativa
+    let dogAge = age * formula;
+
+    resultDiv.innerHTML = dogAge;
 
 }
 
